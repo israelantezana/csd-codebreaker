4461b6ef729e44d4abfd0293cb223e1befeccb65
Israel Antezana Rojas <israelantezana@gmail.com>
2022-07-01 06:10:54 -0400

    Refactoring carga inicial de dom

--- src/codebreaker/codebreaker-ui.test.js
+++ src/codebreaker/codebreaker-ui.test.js
@@ -1,14 +1,15 @@
 import fs from "fs";
 
 describe("Codebreaker UI", () => {
-  it("mostrar el nombre del juego", () => {
+  beforeAll(() => {
     document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
+  });
+  it("mostrar el nombre del juego", () => {
     const h1 = document.querySelector("#titulo");
     expect(h1.innerHTML).toEqual("Codebreaker");
   });
 
   it("No muestra ningun mensaje de ganador al iniciar", () => {
-    document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
     const resultado = document.querySelector("#resultado");
     expect(resultado.innerHTML).toEqual("");
   });
